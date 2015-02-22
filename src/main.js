7877f1782b54b90e0bd849a3d7201beac8d3f377
Joyce Larkins <jhlarkins@users.noreply.github.com>
2015-02-22 01:12:32 -0800

    Navigate with arrow keys.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ var sudoku = require('./sudoku.js');
 var HARD_CODED_VALUE = 'hardCodedValue';
 var SELECTED_VALUE = 'selectedValue';
 
+var UP_ARROW = 38;
+var DOWN_ARROW = 40;
+var LEFT_ARROW = 37;
+var RIGHT_ARROW = 39;
+
 var board;
 var selected;
 
@@ -38,6 +43,21 @@ function selectSquare(row, column) {
     $(toSquareId(row, column)).addClass(SELECTED_VALUE);
     selected = {row: row, column: column};
 }
+
+$(document).keydown(function(event) {
+    var key = event.keyCode;
+    if (selected === undefined) {
+        return;
+    } else if (key === UP_ARROW && selected.row > 0) {
+        selectSquare(selected.row - 1, selected.column);
+    } else if (key === DOWN_ARROW && selected.row < board.size() - 1) {
+        selectSquare(selected.row + 1, selected.column);
+    } else if (key === LEFT_ARROW && selected.column > 0) {
+        selectSquare(selected.row, selected.column - 1);
+    } else if (key === RIGHT_ARROW && selected.column < board.size() - 1) {
+        selectSquare(selected.row, selected.column + 1);
+    }
+});
 
 board = sudoku(3).randomBoard();
 populateBoard();
