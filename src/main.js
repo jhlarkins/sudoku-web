a222ed7c2089b064e56e1c351d27b4abfc26786e
Joyce Larkins <jhlarkins@users.noreply.github.com>
2015-02-22 01:03:02 -0800

    Select cells by clicking on them.

--- src/main.js
+++ src/main.js
@@ -1,26 +1,42 @@
 var $ = require('jquery-browserify');
 var sudoku = require('./sudoku.js');
 
+var HARD_CODED_VALUE = 'hardCodedValue';
+var SELECTED_VALUE = 'selectedValue';
+
 var board;
+var selected;
 
 function toSquareId(row, column) {
-    return 'square_' + row + '_' + column;
+    return '#square_' + row + '_' + column;
 }
 
 function populateBoard() {
     var row;
     var column;
-    var element;
+    var square;
     var value;
     for (row = 0; row < board.size(); row++) {
         for (column = 0; column < board.size(); ++column) {
-            element = $('#' + toSquareId(row, column));
+            square = $(toSquareId(row, column));
             if (board.isHardCoded(row, column)) {
+                square.addClass(HARD_CODED_VALUE);
             }
             value = board.get(row, column);
-            element.text(value !== null ? value : '');
+            square.text(value !== null ? value : '');
+            square.click(selectSquare.bind(null, row, column));
         }
     }
+}
+
+function selectSquare(row, column) {
+    if (selected !== undefined &&
+            !(selected.row === row && selected.column === column)) {
+        $(toSquareId(selected.row, selected.column))
+                .removeClass(SELECTED_VALUE);
+    }
+    $(toSquareId(row, column)).addClass(SELECTED_VALUE);
+    selected = {row: row, column: column};
 }
 
 board = sudoku(3).randomBoard();
