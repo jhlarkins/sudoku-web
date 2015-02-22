e883672d57f5a87f76bfd1d461e2d215c7e6da60
Joyce Larkins <jhlarkins@users.noreply.github.com>
2015-02-22 01:27:19 -0800

    Edit numbers on the board.

--- src/main.js
+++ src/main.js
@@ -9,6 +9,13 @@ var DOWN_ARROW = 40;
 var LEFT_ARROW = 37;
 var RIGHT_ARROW = 39;
 
+var BACKSPACE = 8;
+var DELETE = 46;
+
+var ZERO_CHAR = 48;
+var ONE_CHAR = ZERO_CHAR + 1;
+var NINE_CHAR = ZERO_CHAR + 9;
+
 var board;
 var selected;
 
@@ -56,6 +63,27 @@ $(document).keydown(function(event) {
         selectSquare(selected.row, selected.column - 1);
     } else if (key === RIGHT_ARROW && selected.column < board.size() - 1) {
         selectSquare(selected.row, selected.column + 1);
+    }
+});
+
+$(document).keypress(function(event) {
+    var square;
+    var key;
+    var ch;
+    var num;
+    if (selected === undefined ||
+            board.isHardCoded(selected.row, selected.column)) {
+        return;
+    }
+    square = $(toSquareId(selected.row, selected.column));
+    key = event.keyCode;
+    ch = event.charCode;
+    if (key === BACKSPACE || key === DELETE) {
+        square.text('');
+    } else if (ch >= ONE_CHAR && ch <= NINE_CHAR) {
+        num = ch - ZERO_CHAR;
+        board.set(selected.row, selected.column, num);
+        square.text(num); 
     }
 });
 
